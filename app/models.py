398826b30cd3aa70dcfbdf1a24bad5e62087a170
Paul Winterbauer <paul.winterbauer@gmail.com>
2022-03-17 16:49:35 -0400

    db initialization sqlite

--- app/models.py
+++ app/models.py
@@ -4,14 +4,18 @@ from datetime import datetime
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    event_date = db.Column(db.DateTime, nullable=True)
+    event_door = db.Column(db.DateTime, nullable=True)
+    event_start = db.Column(db.DateTime, nullable=True)
+    event_end = db.Column(db.DateTime, nullable=True)
     artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
-    artist = db.relationship('Artist', foreign_keys=[artist.id])
-    jams = db.relationship('Jam', lazy='dynamic')
+    artist = db.relationship('Artist', foreign_keys=[artist_id])
+    jams = db.relationship('Jam', primaryjoin="Event.id==Jam.event_id", order_by="Jam.track_num")
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'))
+    venue = db.relationship('Venue', foreign_keys=[venue_id])
     updated = db.Column(db.DateTime, index=True, default=datetime.utcnow, nullable=False)
 
     def __repr__(self):
-        return '<Event: {}-{}>'.format(self.title, self.event_date)
+        return '<Event: {}-{}>'.format(self.title, self.event_start)
 
 
 class Artist(db.Model):
@@ -21,6 +25,17 @@ class Artist(db.Model):
     def __repr__(self):
         return '<Artist: {}>'.format(self.name)
 
+class Venue(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    street_address = db.Column(db.String(120))
+    city = db.Column(db.String(64))
+    state = db.Column(db.String(8))
+    zip = db.Column(db.SmallInteger)
+    info = db.Column(db.String(120))
+
+    def __repr__(self):
+        return '<Venue: {}>'.format(self.name)
 
 class Jam(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,7 +43,7 @@ class Jam(db.Model):
     event = db.relationship('Event', foreign_keys=[event_id])
     artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
     artist = db.relationship('Artist', foreign_keys=[artist_id])
-    track_num = db.Column(db.SmallInteger)
+    track_num = db.Column(db.SmallInteger, nullable=False)
     title = db.Column(db.String(120))
 
     def __repr__(self):
