d3e08bdf25c724639265cb4103de65d28a93f7a3
Paul Winterbauer <paul.winterbauer@gmail.com>
2022-03-17 22:52:58 -0400

    added error handling, logging, untested email error handler

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import os
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -8,4 +11,30 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes, models
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = () if app.config['MAIL_USE_TLS'] else None
+        mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                toaddrs=app.config['ADMINS'], subject='JITC Failure',
+                credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('log'):
+        os.mkdir('log')
+    file_handler = RotatingFileHandler('log/jitc.log', maxBytes=10240,
+            backupCount=7)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('JITC starting')
+
+from app import routes, models, errors
