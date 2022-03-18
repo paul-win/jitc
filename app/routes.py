b779a4294ea6ec6e124f46a9e7dcd4eb1cfd4c9c
Paul Winterbauer <paul.winterbauer@gmail.com>
2022-03-18 17:30:52 -0400

    initial bootstrap/css setup, navbar/footer/base template update

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,19 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/events')
+def events():
+    return render_template('index.html', title='Events')
+
+@app.route('/jams')
+def jams():
+    return render_template('index.html', title='Jams')
+
+@app.route('/support')
+def support():
+    return render_template('index.html', title='Support')
+
+@app.route('/mission')
+def mission():
+    return render_template('index.html', title='Mission')
